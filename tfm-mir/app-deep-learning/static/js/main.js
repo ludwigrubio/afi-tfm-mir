31def81347330045c54ca5983aaab94d7ca74a10
Ludwig Gerardo Rubio Jaime <lrubio@alumni.afi.es>
2019-06-23 20:38:40 +0200

    TFM final version

--- tfm-mir/app-deep-learning/static/js/main.js
+++ tfm-mir/app-deep-learning/static/js/main.js
@@ -50,7 +50,7 @@ function smooth(prediction) {
         }
         newPrediction[i] /= totalWeight;
     }
-    return prediction;
+    return newPrediction;
 }
 
 async function process(audio, sampleRate) {
